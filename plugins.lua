3f5a5c39e406d4f6c77d8eb76755a222a1cf297a
IhsenBen <benali.ihsen@gmail.com>
2023-11-21 17:57:17 +0100

    multi line edit

--- plugins.lua
+++ plugins.lua
@@ -105,10 +105,43 @@ local plugins = {
 	-- All NvChad plugins are lazy-loaded by default
 	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
 	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-	-- {
-	--   "mg979/vim-visual-multi",
-	--   lazy = false,
-	-- }
+	{
+		"mg979/vim-visual-multi",
+		keys = {
+			"<C-n>",
+			"<C-N>",
+			"<M-n>",
+			"<S-Down>",
+			"<S-Up>",
+			"<M-Left>",
+			"<M-i>",
+			"<M-Right>",
+			"<M-D>",
+			"<M-Down>",
+			"<C-d>",
+			"<C-Down>",
+			"<C-Up>",
+			"<S-Right>",
+			"<C-LeftMouse>",
+			"<M-LeftMouse>",
+			"<M-C-RightMouse>",
+		},
+		lazy = true,
+		cond = function()
+			return not vim.g.vscode
+		end,
+		init = function()
+			vim.g.VM_mouse_mappings = 1
+			vim.g.VM_silent_exit = 0
+			vim.g.VM_show_warnings = 1
+			vim.g.VM_default_mappings = 1
+
+			vim.cmd([[
+    let g:VM_maps = {}
+    let g:VM_maps["Find Under"] = '<C-b>'
+  ]])
+		end,
+	},
 }
 
 return plugins
